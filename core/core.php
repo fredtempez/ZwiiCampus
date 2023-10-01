73689fc880f85e2bcfb10dcc2fcc034a8cd1c5f1
fredtempez <frederic.tempez@outlook.com>
2023-10-01 21:47:49 +0200

    v6

--- core/core.php
+++ core/core.php
@@ -50,7 +50,7 @@ class common
 	const ACCESS_TIMER = 1800;
 
 	// Numéro de version
-	const ZWII_VERSION = '1.0.05';
+	const ZWII_VERSION = '1.0.06';
 
 	// URL autoupdate
 	const ZWII_UPDATE_URL = 'https://forge.chapril.org/ZwiiCMS-Team/update/raw/branch/master/lms/';
