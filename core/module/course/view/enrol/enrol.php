06a9a1b004ebdabdd1407fa89269361a9d4196f3
fredtempez <frederic.tempez@outlook.com>
2023-10-06 18:15:57 +0200

    fix enrol display banner

--- core/module/course/view/enrol/enrol.php
+++ core/module/course/view/enrol/enrol.php
@@ -2,9 +2,14 @@
 <div class="row">
     <div class="col12">
         <?php echo "<h3>Auteur : " . $this->getData(['course', $this->getUrl(2), 'author']) . "</h3>"; ?>
-        <?php echo "<p> Description : " . $this->getData(['course', $this->getUrl(2), 'description']) . "</p>"; ?>
-        <?php echo "<p> Disponibilité : " . $module::$courseAccess[$this->getData(['course', $this->getUrl(2), 'access'])] . "</p>"; ?>
-        <?php echo "<p> Inscription : " . $module::$courseEnrolment[$this->getData(['course', $this->getUrl(2), 'enrolment'])] . "</p>"; ?>
+        <?php echo "<p>Description : " . $this->getData(['course', $this->getUrl(2), 'description']) . "</p>"; ?>
+        <?php echo "<p>Disponibilité : " . $module::$courseAccess[$this->getData(['course', $this->getUrl(2), 'access'])] ; ?>
+        <?php if ($this->getData(['course', $this->getUrl(2), 'access']) === $module::COURSE_ACCESS_DATE): ?>
+            <?php $from = helper::dateUTF8('%d %B %Y', $this->getData(['course', $this->getUrl(2), 'openingDate']), self::$i18nUI) . helper::translate(' à ') . helper::dateUTF8('%H:%M', $this->getData(['course', $this->getUrl(2), 'openingDate']), self::$i18nUI); ?>
+            <?php $to = helper::dateUTF8('%d %B %Y', $this->getData(['course', $this->getUrl(2), 'closingDate']), self::$i18nUI) . helper::translate(' à ') . helper::dateUTF8('%H:%M', $this->getData(['course', $this->getUrl(2), 'closingDate']), self::$i18nUI); ?>
+            <?php echo sprintf(helper::translate(' du %s au %s'), $from, $to); ?>
+        <?php endif; ?>
+        <?php echo "</p><p>Inscription : " . $module::$courseEnrolment[$this->getData(['course', $this->getUrl(2), 'enrolment'])] . "</p>"; ?>
     </div>
 </div>
 <div class="row">
