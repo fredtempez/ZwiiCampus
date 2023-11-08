106ace8cef7c737cd51a2712983db876f09894cb
fredtempez <frederic.tempez@outlook.com>
2023-11-08 11:47:50 +0100

    v67

--- core/core.php
+++ core/core.php
@@ -50,7 +50,7 @@ class common
 	const ACCESS_TIMER = 1800;
 
 	// Numéro de version
-	const ZWII_VERSION = '1.0.66';
+	const ZWII_VERSION = '1.0.67';
 
 	// URL autoupdate
 	const ZWII_UPDATE_URL = 'https://forge.chapril.org/ZwiiCMS-Team/campus-update/raw/branch/master/';
