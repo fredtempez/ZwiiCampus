ee362aa3fb761b80f1cad915cdeb637e741ebaf1
Fred Tempez <frederic.tempez@outlook.com>
2023-11-29 18:12:22 +0100

    supprimer le cookie SITE_CONTENT

--- core/core.php
+++ core/core.php
@@ -390,7 +390,7 @@ class common
 		}
 		// Stocker le cookie de langue pour l'éditeur de texte ainsi que l'url du contenu pour le theme
 		setcookie('ZWII_UI', self::$i18nUI, time() + 3600, '', '', false, false);
-		setcookie('ZWII_SITE_CONTENT', self::$siteContent, time() + 3600, '', '', false, false);
+		//setcookie('ZWII_SITE_CONTENT', self::$siteContent, time() + 3600, '', '', false, false);
 		setlocale(LC_ALL, self::$i18nUI);
 
 		// Construit la liste des pages parents/enfants
