9c32f1a560fc231b3196fddf4724c24b05dd040a
F TEMPEZ <frederic.tempez@outlook.com>
2023-12-14 14:50:03 +0100

    1400

--- core/core.php
+++ core/core.php
@@ -50,7 +50,7 @@ class common
 	const ACCESS_TIMER = 1800;
 
 	// Numéro de version
-	const ZWII_VERSION = '1.3.04';
+	const ZWII_VERSION = '1.4.00';
 
 	// URL autoupdate
 	const ZWII_UPDATE_URL = 'https://forge.chapril.org/ZwiiCMS-Team/campus-update/raw/branch/master/';
