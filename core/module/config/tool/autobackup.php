d35010f100b8e635f5be370569bad5a074b6b888
fredtempez <frederic.tempez@outlook.com>
2024-03-17 12:46:38 +0100

    Livre un outil de mise à jour automatisé appelable par CRON

---
+++ core/module/config/tool/autobackup.php
@@ -0,0 +1,26 @@
+<?php
+// Creation du ZIP
+$filter = $filter = ['backup', 'tmp'];
+$fileName = 'Campus-Rolling' . '-backup-' . date('Y-m-d-H-i-s', time()) . '.zip';
+$zip = new ZipArchive();
+$zip->open('../../../../site/backup/' . $fileName, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+$directory = '../../../../site';
+$files = new RecursiveIteratorIterator(
+    new RecursiveCallbackFilterIterator(
+        new RecursiveDirectoryIterator(
+            $directory,
+            RecursiveDirectoryIterator::SKIP_DOTS
+        ),
+        function ($fileInfo, $key, $iterator) use ($filter) {
+            return $fileInfo->isFile() || !in_array($fileInfo->getBaseName(), $filter);
+        }
+    )
+);
+foreach ($files as $name => $file) {
+    if (!$file->isDir()) {
+        $filePath = $file->getRealPath();
+        $relativePath = substr($filePath, strlen(realpath($directory)) + 1);
+        $zip->addFile($filePath, $relativePath);
+    }
+}
+$zip->close();
