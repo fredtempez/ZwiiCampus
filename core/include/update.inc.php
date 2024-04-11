44c4997436ce56fb55c9a175ee6abd793c5c1a30
Fred Tempez <frederic.tempez@outlook.com>
2024-04-11 07:55:48 +0200

    upodaite to 1.8 WIP

--- core/include/update.inc.php
+++ core/include/update.inc.php
@@ -21,3 +21,33 @@ if (
     }
     $this->setData(['core', 'dataVersion', 1700]);
 }
+
+
+if (
+    $this->getData(['core', 'dataVersion']) < 1800
+) {
+    // Déplace les historiques dans les dossiers des esapaces 
+    // Parcourir les espaces
+    foreach ($this->getData(['course']) as $courseId => $courseValues) {        
+        $data = [];
+        //Parcourir les participants
+        foreach ($this->getData(['user']) as $userId => $userValues) {
+            // Un historique existe pour ce participant
+            $report = $this->getData(['enrolment', $courseId, $userId, 'history']);
+            if ( is_array($report)
+            ) {
+                // Ecriture dans un fichier report dans le dossier de l'espace
+                $data[$userId] = array_merge($data, [$userId => $report]);
+                // Nettoyage du fichier des inscriptions
+                // Ce fichier ne contient que l'id du participant et de la date et de l'id de la dernière page vue
+               // $this->deleteData(['enrolment', $courseId, $userId, 'history']);
+            } 
+        }
+        // Stocke le rapport en CSV
+        $file = fopen(self::DATA_DIR . $courseId . '/report.csv', 'a+');
+        fputcsv($file,  [$data], ';');
+        fclose($file);
+    } 
+    //$this->setData(['core', 'dataVersion', 1800]);
+
+}
