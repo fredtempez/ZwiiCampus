fea1c40a4b0cffdf482929e0fddc14b3b740ac5d
Fred Tempez <frederic.tempez@outlook.com>
2024-09-06 09:08:21 +0200

    Bug datatables ajout dans user

--- core/module/user/view/index/index.js.php
+++ core/module/user/view/index/index.js.php
@@ -29,6 +29,7 @@ $(document).ready((function () {
         },
         locale: 'fr',
         stateSave: true,
+        "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "Tout"]],
         "columnDefs": [
             {
                 target: 5,
