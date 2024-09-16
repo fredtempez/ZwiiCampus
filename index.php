35521766cda751f31446ea53f6acd2b578c5e7d9
Frédéric Tempez <frederic.tempez@outlook.com>
2024-09-16 21:20:35 +0200

    1.12.06 Chemin du cookie de session

--- index.php
+++ index.php
@@ -14,19 +14,38 @@
  * @link http://zwiicms.fr/
  */
 
-/**
+ /**
  * Initialisation de Zwii
  */
 // Remplace la directive htaccess
 ini_set('session.use_trans_sid', FALSE);
+
 // Crée un identifiant unique pour chaque site en fonction du nom de domaine ou autre
 $siteId = md5($_SERVER['SERVER_NAME'] . $_SERVER['SCRIPT_FILENAME']); // Ou utilise un autre identifiant unique pour chaque site
 // Change le nom de la session en fonction de cet identifiant
 session_name('zwii_session_' . $siteId);
+
+// Récupère dynamiquement le chemin du dossier dans lequel le script est exécuté
+$scriptPath = dirname($_SERVER['SCRIPT_NAME']) . '/';
+
+// Définissez le chemin du cookie de session dynamiquement
+session_set_cookie_params([
+    'lifetime' => 0,
+    'path' => $scriptPath, // Utilise le chemin du script pour restreindre la session à ce répertoire
+    'domain' => $_SERVER['SERVER_NAME'], // Domain par défaut
+    'secure' => isset($_SERVER['HTTPS']), // Pour HTTPS, si nécessaire
+    'httponly' => true,
+    'samesite' => 'Lax' // Ou 'Strict' ou 'None' selon tes besoins
+]);
+
 // Démarre la session
 session_start();
+
 // Contrôle des conditions de fonctionnement
 include_once('core/include/checkup.php');
+
+// Mise à jour du système de BDD
+include_once('core/include/pre-update.php');
 
 /*
  *Localisation par défaut 
@@ -37,17 +56,17 @@ include_once('core/include/checkup.php');
  * fr_FR.utf8 : la majorité
 */
 date_default_timezone_set('Europe/Paris');
-setlocale(LC_ALL, 'fr_FR.UTF8', 'fr_FR', 'french');
+setlocale (LC_ALL, 'fr_FR.UTF8', 'fr_FR', 'french');
 
 /**
  * Chargement des classes
  */
 require 'core/class/autoload.php';
 autoload::autoloader();
-spl_autoload_register('core::autoload');
 
 /**
  * Chargement du coeur
  */
 $core = new core;
+spl_autoload_register('core::autoload');
 echo $core->router();
