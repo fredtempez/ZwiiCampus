54f0b0c6e38ea9717c221e6698563b4a87d55500
Frédéric Tempez <frederic.tempez@outlook.com>
2024-10-02 12:12:52 +0200

    redondance de déclaration de charset

--- core/layout/mail.php
+++ core/layout/mail.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="fr_FR">
 <head>
 	<meta charset="UTF-8">
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta http-equiv="content-type" content="text/html">
 	<meta name="viewport" content="width=device-width">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="x-apple-disable-message-reformatting">
