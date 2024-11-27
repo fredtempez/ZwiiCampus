130b88ace1213c28de75e5314e05a0686bb7746f
Frédéric Tempez <frederic.tempez@outlook.com>
2024-11-27 14:09:50 +0100

    1.14.07 Mémorisation de la position de l'onglet dans la configuration

--- core/module/config/view/index/index.php
+++ core/module/config/view/index/index.php
@@ -19,22 +19,22 @@
     <?php echo template::button('configSetupButton', [
         'value' => 'Configuration',
         'class' => 'buttonTab',
-		//'href' => helper::baseUrl() . 'config/register/setup'
+		'href' => helper::baseUrl() . 'config/register/setup'
     ]); ?>
     <?php echo template::button('configSocialButton', [
         'value' => 'Référencement',
         'class' => 'buttonTab',
-		//'href' => helper::baseUrl() . 'config/register/social'
+		'href' => helper::baseUrl() . 'config/register/social'
     ]); ?>
     <?php echo template::button('configConnectButton', [
         'value' => 'Connexion',
         'class' => 'buttonTab',
-		//'href' => helper::baseUrl() . 'config/register/connect'
+		'href' => helper::baseUrl() . 'config/register/connect'
     ]); ?>
     <?php echo template::button('configNetworkButton', [
         'value' => 'Réseau',
         'class' => 'buttonTab',
-		//'href' => helper::baseUrl() . 'config/register/network' 
+		'href' => helper::baseUrl() . 'config/register/network' 
     ]); ?>
 </div>
 <?php include('core/module/config/view/locale/locale.php') ?>
