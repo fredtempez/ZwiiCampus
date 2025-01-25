dd3582b9dbd67b6415e02f59c095375271067ef9
Frédéric Tempez <frederic.tempez@outlook.com>
2025-01-25 22:25:12 +0100

    Supprime des commentaires

--- core/module/page/view/edit/edit.php
+++ core/module/page/view/edit/edit.php
@@ -35,27 +35,22 @@
 	<?php echo template::button('pageEditContentButton', [
 		'value' => 'Contenu',
 		'class' => 'buttonTab',
-		//'href' => helper::baseUrl() . 'page/register/content/' . $this->geturl(2)
 	]); ?>
 	<?php echo template::button('pageEditPositionButton', [
 		'value' => 'Menu',
 		'class' => 'buttonTab',
-		//'href' => helper::baseUrl() . 'page/register/position/' . $this->geturl(2)
 	]); ?>
 	<?php echo template::button('pageEditExtensionButton', [
 		'value' => 'Extension',
 		'class' => 'buttonTab',
-		//'href' => helper::baseUrl() . 'page/register/extension/' . $this->geturl(2)
 	]); ?>
 	<?php echo template::button('pageEditLayoutButton', [
 		'value' => 'Mise en page',
 		'class' => 'buttonTab',
-		//'href' => helper::baseUrl() . 'page/register/layout/' . $this->geturl(2)
 	]); ?>
 	<?php echo template::button('pageEditPermissionButton', [
 		'value' => 'Permission',
 		'class' => 'buttonTab',
-		//'href' => helper::baseUrl() . 'page/register/permission/' . $this->geturl(2)
 	]); ?>
 </div>
 
