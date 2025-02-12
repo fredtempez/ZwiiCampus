4dccc2c37692d55c951a3f6e4eee78e872f681ad
Frédéric Tempez <frederic.tempez@outlook.com>
2025-02-12 12:13:13 +0100

    bug group -> role

--- core/module/course/course.php
+++ core/module/course/course.php
@@ -366,7 +366,7 @@ class course extends common
         // Liste des enseignants pour le sélecteur d'auteurs
         $teachers = $this->getData(['user']);
         foreach ($teachers as $teacherId => $teacherInfo) {
-            if ($teacherInfo['group'] >= 2) {
+            if ($teacherInfo['role'] >= 2) {
                 self::$courseTeachers[$teacherId] = $teacherInfo['firstname'] . ' ' . $teacherInfo['lastname'];
             }
         }
