1f91f501d5ac99977bab494137311fdc75901978
Frédéric Tempez <frederic.tempez@outlook.com>
2025-02-12 10:37:54 +0100

    Bugs getUser getUrl

--- core/core.php
+++ core/core.php
@@ -641,7 +641,7 @@ class common
 	/**
 	 * Ecrire les données de la page
 	 * @param string pageId
-	 * @param array contenu de la page
+	 * @param string contenu de la page
 	 * @return int nombre d'octets écrits ou erreur
 	 */
 	public function setPage($page, $value, $path)
@@ -1663,7 +1663,7 @@ function makeThumb($src, $dest, $desired_width)
 				}
 				return $filter;
 			default:
-				return null;
+				return [];
 		}
 	}
 
