79bfbc4d67273042f060f82de22bda88b66f61b6
Frédéric Tempez <frederic.tempez@outlook.com>
2025-02-27 13:05:02 +0100

    Animation dans course / manage

--- core/module/course/view/manage/manage.js.php
+++ core/module/course/view/manage/manage.js.php
@@ -11,24 +11,85 @@
 * @link http://zwiicms.fr/
 */
 
-/**
+
+
+
+$(document).ready(function () {
+
+  /**
   * Confirmation de suppression
   */
 $(".courseDelete").on("click", function () {
-    var _this = $(this);
-    var message = "<?php echo helper::translate('Supprimer cet espace et les documents du gestionnaire de fichier ?'); ?>";
-    return core.confirm(message, function () {
-        $(location).attr("href", _this.attr("href"));
-    });
-});
-
-/**
-  * Confirmation de reset
-  */
- $(".courseReset").on("click", function () {
   var _this = $(this);
-  var message = "<?php echo helper::translate('Réinitialiser cet espace ?'); ?>";
+  var message = "<?php echo helper::translate('Supprimer cet espace et les documents du gestionnaire de fichier ?'); ?>";
   return core.confirm(message, function () {
       $(location).attr("href", _this.attr("href"));
   });
 });
+
+/**
+* Confirmation de reset
+*/
+$(".courseReset").on("click", function () {
+var _this = $(this);
+var message = "<?php echo helper::translate('Réinitialiser cet espace ?'); ?>";
+return core.confirm(message, function () {
+    $(location).attr("href", _this.attr("href"));
+});
+});
+
+
+
+	// Gestion de l'affichage de la période d'ouverture
+	function togglePeriodSetup() {
+		if ($('#courseManageAccess').val() == 1) {
+			$('.periodSetup').slideDown();
+		} else {
+			$('.periodSetup').slideUp();
+		}
+	}
+
+	// Afficher ou masquer la date limite d'inscription
+	function toggleEnrolmentLimitDate() {
+		if ($('#courseManageEnrolmentLimit').is(':checked')) {
+			$('#courseManageEnrolmentLimitDateWrapper').slideDown();
+		} else {
+			$('#courseManageEnrolmentLimitDateWrapper').slideUp();
+		}
+	}
+
+	// Vérifie que la date limite d'inscription est valide
+	function validateEnrolmentLimitDate() {
+		let openingDate = new Date($('#courseOpeningDate').val());
+		let closingDate = new Date($('#courseClosingDate').val());
+		let enrolmentDate = new Date($('#courseManageEnrolmentLimitDate').val());
+
+		// Vérifie si les dates sont valides
+		if (isNaN(openingDate.getTime()) || isNaN(closingDate.getTime()) || isNaN(enrolmentDate.getTime())) {
+			return; // Ne fait rien si une des dates est vide ou invalide
+		}
+
+		// Vérifie que la date d'inscription est entre les deux bornes
+		if (enrolmentDate < openingDate || enrolmentDate > closingDate) {
+			alert("La date limite d'inscription doit être entre la date d'ouverture et de fermeture.");
+			$('#courseManageEnrolmentLimitDate').val(''); // Réinitialise l'input
+		}
+	}
+
+	// Fonction pour afficher ou masquer le champ courseManageEnrolmentKey
+	function toggleEnrolmentKey() {
+		if ($('#courseManageEnrolment').prop('selectedIndex') == 2) {
+			$('#courseManageEnrolmentKeyWrapper').slideDown();
+		} else {
+			$('#courseManageEnrolmentKeyWrapper').slideUp();
+		}
+	}
+
+
+	// Initialisation
+	togglePeriodSetup();
+	toggleEnrolmentLimitDate();
+	toggleEnrolmentKey();
+
+});
+
