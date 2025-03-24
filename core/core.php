b6942c01150cec67e4f57d64ded94309cd4a0074
Frédéric Tempez <frederic.tempez@outlook.com>
2025-03-24 13:54:22 +0100

    Version number

--- core/core.php
+++ core/core.php
@@ -62,7 +62,7 @@ class common
 	const ACCESS_TIMER = 1800;
 
 	// Numéro de version
-	const ZWII_VERSION = '2.0.02';
+	const ZWII_VERSION = '2.1.00';
 
 	// URL autoupdate
 	const ZWII_UPDATE_URL = 'https://forge.chapril.org/ZwiiCMS-Team/campus-update/raw/branch/master/';
